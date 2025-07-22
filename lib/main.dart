6fb268cbde78798efe71bfa0df7ac000702280b6
Agung Kurniawan <agungklewang26@gmail.com>
2025-07-22 19:44:52 +0700

    riverpod main.dart syntax changes

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,14 @@ void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Batik Sesa Bojonegoro',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
       onGenerateRoute: AppRoutes.generateRoute,
       home: const SplashScreen(),
     );
